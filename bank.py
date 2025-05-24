5c1a60ebd1b064e5ddd6767e85e6be6a2d5676d3
Ali <ayayici99@gmail.com>
2025-05-24 13:58:46 +0300

    Update bank.py

--- bank.py
+++ bank.py
@@ -1,12 +1,15 @@
+import datetime
+def get_time():
+    return datetime.datetime.now().strftime("%X")
 def create_account(owner_name):
     return {"owner":owner_name,"balance":0}
-def çekme(account,amount):
+def yatırma(account,amount):
     if amount>0:
         account["balance"]+=amount    
         print(f"{amount} TL başarılı bir şekilde yatırıldı. güncel bakiyeniz:{account["balance"]} TL")
     else:
         print("yatırılacak miktar pozitif olmalı")
-def yatırma(account,amount):
+def çekme(account,amount):
     if amount>0:
         if account["balance"]>=amount:
            account["balance"]-=amount
@@ -18,8 +21,9 @@ def yatırma(account,amount):
 def balance(account):
     print(f"{account["owner"]} hesabının güncel bakiyesi:{account["balance"]} TL")
 
-print("bankamızın hesap uygulamasına hoş geldiniz!")
+print("bankamızın hesap uygulamasına hoş geldiniz!  (Giriş:{get_time()})")
 owner=input("hesap sahibinin adı:")
+password=input("lütfen şifrenizi giriniz:")
 account=create_account(owner)
 
 while(True):
@@ -31,14 +35,14 @@ while(True):
     choice=int(input("seçiminizi yapınız (1-4):"))
     if choice==1:
         amount=float(input("yatırmak istediğiniz miktarı giriniz:"))
-        çekme(account,amount)
+        yatırma(account,amount)
     elif choice==2:
         amount=float(input("çekmek istediğiniz miktarı giriniz:"))    
-        yatırma(account,amount)
+        çekme(account,amount)
     elif choice==3:
         balance(account)    
     elif choice==4:
-        print("uygulamadan çıktınız. iyi günler dileriz")    
+        print("uygulamadan çıktınız. iyi günler dileriz  (Çıkış:{get_time()})")    
         break
     else:
         print("geçersiz seçim.lütfen 1-4 arasında bir seçim yapınız ")
