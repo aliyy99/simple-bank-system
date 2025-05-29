1a96cbe84f672e3bf98bc3deccaa7f7c22040bd5
Ali <ayayici99@gmail.com>
2025-05-29 15:31:44 +0300

    Update bank.py

--- bank.py
+++ bank.py
@@ -3,52 +3,52 @@ def get_time():
     return datetime.datetime.now().strftime("%X")
 def create_account(owner_name):
     return {"owner":owner_name,"balance":0}
-def yatırma(account,amount):
+def deposit(account,amount):
     if amount>0:
         account["balance"]+=amount    
-        print(f"{amount} TL başarılı bir şekilde yatırıldı. güncel bakiyeniz:{account["balance"]} TL")
+        print(f"{amount} TL deposited successfully. Current balance:{account["balance"]} TL")
     else:
-        print("yatırılacak miktar pozitif olmalı")
-def çekme(account,amount):
+        print("the amount to be deposited must be positive")
+def withdraw(account,amount):
     if amount>0:
         if account["balance"]>=amount:
            account["balance"]-=amount
-           print(f"{amount} TL başarılı bir şekilde çekildi. güncel bakiyeniz:{account["balance"]} TL")         
+           print(f"{amount} TL successfully withdrawn. Current balance:{account["balance"]} TL")         
         else:
-            print("yetersiz bakiye.")   
+            print("İnsufficient balance.")   
     else:
-        print("çekilecek miktar pozitif olmalı")        
+        print("the amount to be withdrawn must be positive")        
 def balance(account):
-    print(f"{account["owner"]} hesabının güncel bakiyesi:{account["balance"]} TL")
+    print(f"current balance of the {account["owner"]} account:{account["balance"]} TL")
 
-print("bankamızın hesap uygulamasına hoş geldiniz!  (Giriş:{get_time()})")
-owner=input("hesap sahibinin adı:")
+print("Welcome to our bank account application!  (Access:{get_time()})")
+owner=input("Name and surname of the account holder:")
 while True:
-    entered_pass=input("Lütfen 6 haneli rakamlardan oluşan şifrenizi giriniz:")
+    entered_pass=input("Please enter your password consisting of 6 digits:")
     if entered_pass.isdigit() and len(entered_pass)==6:
-       print("tebrikler şifreyi doğru girdiniz.")  
+       print("Congratulations! You entered the password correctly.")  
        break 
     else:
-       print("Hatalı giriş! şifreniz sadece 6 haneli rakamlardan oluşmaktadır.")
+       print("Incorrect login! Your password consists of only 6 digits.")
 account=create_account(owner)
 
 while(True):
-    print("\nseçenekler:")
-    print("1.para yatır")
-    print("2.para çek")
-    print("3.bakiye görüntüle")
-    print("4.çıkış")
-    choice=int(input("seçiminizi yapınız (1-4):"))
+    print("\nOptions:")
+    print("1.Deposit Money")
+    print("2.Withdraw Money")
+    print("3.View Balance")
+    print("4.Exit")
+    choice=int(input("Make your choice (1-4):"))
     if choice==1:
-        amount=float(input("yatırmak istediğiniz miktarı giriniz:"))
-        yatırma(account,amount)
+        amount=float(input("Enter the amount you want to deposit:"))
+        deposit(account,amount)
     elif choice==2:
-        amount=float(input("çekmek istediğiniz miktarı giriniz:"))    
-        çekme(account,amount)
+        amount=float(input("Enter the amount you want to withdraw:"))    
+        withdraw(account,amount)
     elif choice==3:
         balance(account)    
     elif choice==4:
-        print("uygulamadan çıktınız. iyi günler dileriz  (Çıkış:{get_time()})")    
+        print("You are out of the app. Have a nice day!  (Exit:{get_time()})")    
         break
     else:
-        print("geçersiz seçim.lütfen 1-4 arasında bir seçim yapınız ")
+        print("İnvalid selection. Please make a selection between 1-4. ")
